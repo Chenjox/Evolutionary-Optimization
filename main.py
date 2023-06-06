eef9c242625c9ab0959aa5baae74427349c097f3
Kryol1th <nicolaswinkler.nw@gmail.com>
2023-06-06 23:38:18 +0200

    Visualisierung

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import numpy as np
 import scipy as sci
 from scipy.stats import truncnorm
 import random
+import matplotlib
+from matplotlib import pyplot as plt
 
 # Globale Variablen
 # Dimension der Zielfunktion
 dimension = 3
+# Arrays zur Visualiserung:
+rectangelXYZ = np.zeros([1,3])
+rectangelHeight = np.zeros(1)
+rectangleWidth = np.zeros(1)
 
 # Zielfunktion, 
 # xVec: ein N-Dimensionaler Vector der Eingangsgrößen
@@ -75,11 +81,15 @@ def getStartpunkte(mittelpunkt,maxAbstand,anzahlPunkte):
     return p
 
 
-        
-
+def dataToVisualize(XYZ, Height, Width):
+    # Arrays zur Visualiserung:
+    global rectangleWidth, rectangelXYZ
+    rectangelXYZ = np.vstack((rectangelXYZ, XYZ))
+    rectangleWidth = np.append(rectangleWidth, Width)
 
 # startPoint: N-Dimensionaler Vector
 # minMax: Boolean ob minimum oder Maximum gesucht ist
+
 def optimize(isMaximumsuche,anzahlStartPunkte, minBereich,maxBereich,minSuchbereich,maxSuchbereich,verkleinerungsfaktor):
 
     minSB = minSuchbereich
@@ -125,6 +135,10 @@ def optimize(isMaximumsuche,anzahlStartPunkte, minBereich,maxBereich,minSuchbere
             momentanePunkte = elternpunkt + suchpunkte
             print("Ich verkleinere mich!")
 
+
+            #Speichern der relevanten Daten zur Visualiserung
+            dataToVisualize(punkte[0], maxSuchbereich, maxSuchbereich)
+
         momentanePunkte = forceBoundaries(momentanePunkte,minBereich,maxBereich)
         itere = itere + 1
         
@@ -133,11 +147,25 @@ def optimize(isMaximumsuche,anzahlStartPunkte, minBereich,maxBereich,minSuchbere
 
 ## Erster Suchbereich ist abhängig von dem Eingangsraum.
 
-
 ## Hauptprogramm
 
 #zfOpt = optimize()
 
 p = optimize(True,5,-math.pi,math.pi, 0.1,0.8 ,0.3)
 print(p)
-# print(p)
+
+
+
+#Visualisierung
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal')
+plt.xlim([-1, 3])
+plt.ylim([-1, 3])
+n = 10
+#(np.size(rectangleWidth))
+for i in range(1, n):
+    x = rectangelXYZ[i, 0]
+    y = rectangelXYZ[i, 1]
+    w = rectangleWidth[i]
+    ax.add_patch(matplotlib.patches.Rectangle((x, y), w, w, fill=None, alpha=1))
+plt.show()
